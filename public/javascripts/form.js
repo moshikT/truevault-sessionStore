5c2e904ce8124813cf7fef0430833c63f936530e
moshik <moshik.tsadok@gmail.com>
2017-10-23 15:30:02 +0300

    merge with Amit

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -163,3 +163,17 @@ function getCid() {
     var cid = urlPath.split('/');
     return cid[1];
 }
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
