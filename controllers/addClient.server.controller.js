1d33cfa6c5f008a42aa32f1f9ec763036e568be2
amit <amit@empiricalhire.com>
2017-11-16 20:08:02 +0200

    Fixed issues with updating an existing client. Added nice errors in certain failure cases.

--- controllers/addClient.server.controller.js
+++ controllers/addClient.server.controller.js
@@ -17,89 +17,101 @@ class userData {
 }
 */
 exports.getAddClientPage = function (req, res) {
-    res.render('addClient', { title: 'Add New Client', client: ''});
+    res.render('addClient', {title: 'Add New Client', client: ''});
 }
 
 exports.addClient = function (req, res) {
     var logoImg = {};
-    if(req.file) {
+    if (req.file) {
         logoImg.fileName = req.file.originalname;
         logoImg.data = fs.readFileSync(req.file.path);
         logoImg.contentType = 'image/png';
     }
 
+    //console.log("%s.%s:%s -", __file, __ext, __line, "Request body: ", req.body);
     var companyName = req.body.name;
 
-    var url = req.protocol + '://' + req.get('host') + '/clients/' + req.client._id;//+ req.originalUrl;
-    console.log("%s.%s:%s -", __file, __ext, __line, url);
+    // Search for this client in the db
+    Client.findOne({name: companyName.trim()}, function (err, client) { // callback
+        if (err) { // Error checking the db - abort
+            res.render('niceError', {
+                title: 'Add Client',
+                errorText: 'Add client failed!'
+            });
+            return;
+        }
 
-    generateLink(url, function(shortendLink) {
-       // console.log("%s.%s:%s -", __file, __ext, __line, "generate Link " , shortendLink);
-        var newClientEntry = new Client ({
-            name: companyName.trim(),
-            logoImg : logoImg,
-            logoStyle : req.body.logoStyle,
-            title : req.body.title,
-            headlineText: req.body.headlineText,
-            companyDescription : req.body.companyDescription,
-            instructionText: req.body.instructionText,
-            thankYouText: req.body.thankYouText,
-            language : req.body.language,
-            isDemo : (req.body.isDemo == 'on'),
-            link: shortendLink,
-            keyword: req.body.questionsKeyword,
+        let newClientEntry; // place to hold found client or new client entry (if not found)
+        if (client) { // found this client in db
+            newClientEntry = client;
+        }
+        else { // didn't find
+            // prepare a new entry
+            newClientEntry = new Client({
+                name: companyName.trim()
+            });
+        }
+
+        // Save all the fields that were entered in the form
+        if (req.body.logoStyle) {
+            newClientEntry.logoStyle = req.body.logoStyle;
+        }
+        if (req.body.title) {
+            newClientEntry.title = req.body.title;
+        }
+        if (req.body.headlineText) {
+            newClientEntry.headlineText = req.body.headlineText;
+        }
+        if (req.body.companyDescription) {
+            newClientEntry.companyDescription = req.body.companyDescription;
+        }
+        if (req.body.instructionText) {
+            newClientEntry.instructionText = req.body.instructionText;
+        }
+        if (req.body.language) {
+            newClientEntry.language = req.body.language;
+        }
+        if (req.body.isDemo) {
+            newClientEntry.isDemo = (req.body.isDemo == 'on');
+        }
+        if (req.body.questionsKeyword) {
+            newClientEntry.keyword = req.body.questionsKeyword;
+        }
+        if (req.file) {
+            newClientEntry.markModified('logoImg');
+            newClientEntry.logoImg = logoImg;
+            console.log("%s.%s:%s -", __file, __ext, __line, "file exists!!", client.logoImg);
+        }
+        newClientEntry.save(function (err) {
+            if (err) {
+                console.log("%s.%s:%s -", __file, __ext, __line, err);
+                res.render('niceError', {
+                    title: 'Add Client',
+                    errorText: 'Saving client failed!'
+                });
+                return;
+            }
+            else {
+                console.log("%s.%s:%s -", __file, __ext, __line, "Saved client data");
+                res.redirect('addClient');
+            }
         });
 
-        Client.findOne({name: companyName.trim()}, function (err, client) { // callback
-                if (err) {
-                    throw err;
-                }
-                if (client) {
-                    for(var field in req.body ) {
-                        console.log("%s.%s:%s -", __file, __ext, __line, field);
-                        /* Update only unempty fields */
-                         if(newClientEntry[field] != '') {
-                             client[field] = newClientEntry[field];// handle document
-                         }
-                    }
-                    if(req.file) {
-                        client.markModified('logoImg');
-                        client.logoImg = newClientEntry['logoImg'];
-                        console.log("%s.%s:%s -", __file, __ext, __line, "file exists!!", client.logoImg);
-                    }
-                    client.save(function (err) {
-                        if (err) {
-                            console.log("%s.%s:%s -", __file, __ext, __line, err);
-                        }
-                        else {
-                            console.log("%s.%s:%s -", __file, __ext, __line, "update new client! ");
-                        }
-                    });
-                }
-                else {
-                     newClientEntry.save(function (err) {
-                         if (err) {
-                             console.log("%s.%s:%s -", __file, __ext, __line, err);
-                         }
-                         else {
-                             console.log("%s.%s:%s -", __file, __ext, __line, "save new client! ");
-                         }
-                     });
-                 }
-         });
-     });
-    res.redirect('/addClient');
-}
+    });
 
-exports.loadClient = function(req, res) {
+
+};
+
+
+exports.loadClient = function (req, res) {
     var clientName = req.body.clientName;
     //var filterbyClientName = "\"" + clientName + "\""
 
     //var query = Client.find({$text: {$search: filterbyClientName}});
     var query = Client.findOne({name: clientName.trim()});
-    query.exec(function(err, client) {
-        console.log("%s.%s:%s -", __file, __ext, __line, "filter results: ",  client);
-        if(client) {
+    query.exec(function (err, client) {
+        // console.log("%s.%s:%s -", __file, __ext, __line, "filter results: ", client);
+        if (client) {
             res.render('addClient', {title: 'Update Client', client: client});
         }
         else {
