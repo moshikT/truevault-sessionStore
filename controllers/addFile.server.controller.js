67deca22fc1bb9de14a4ed62b20fc786a229edec
amit <amit@empiricalhire.com>
2017-11-17 12:24:51 +0200

    Code review and documentation for addFile.server.controller

--- controllers/addFile.server.controller.js
+++ controllers/addFile.server.controller.js
@@ -4,18 +4,24 @@ const fs = require('fs');
 const path = require('path');
 const DataFile = require('../models/file.server.model.js');
 
+// Display the file uploading page
 exports.getAddFilePage = function (req, res) {
     res.render('addFile', {title: 'Add File', stage: 0});
 };
 
+// Display the file uploading response in error state
+//FIXME: In this case the upload form should be displayed with the data from before and an error message
 function renderError(req, res) {
     res.render('addFile', {title: 'Add File', stage: -1});
 }
 
+// Display the file uploading response in success state
+//FIXME: In this case the upload form should be displayed with the data from before and a success message
 function renderSuccess(req, res) {
     res.render('addFile', {title: 'Add File', stage: 1});
 }
 
+// Handler for POST of upload file form
 exports.addFile = function (req, res) {
     if (!req.file) { // safety
         console.log("%s.%s:%s -", __file, __ext, __line, "req.file: ", req.file);
@@ -26,15 +32,13 @@ exports.addFile = function (req, res) {
         return renderError(req, res);
     }
 
+    // The filename to be used to store the file in the DB - provided in the form
     const newFileName = req.body.fileName;
 
-    const dataFileEntry = new DataFile({
-        fileData: fs.readFileSync(req.file.path),
-        name: newFileName
-    });
-
+    // Check if this file is already stored in the DB
     DataFile.findOne({fileName: newFileName}, function (err, dataFileFound) {
-        if (err) {
+        if (err) { // There was an error checking the DB
+            // In this case we can't store the file because we may be creating duplicate entries
             console.log("%s.%s:%s -", __file, __ext, __line, "Error: ", err);
             return renderError(req, res);
         }
@@ -44,14 +48,16 @@ exports.addFile = function (req, res) {
         if (dataFileFound) { // This file is already in collection so need to replace it
             dataFileEntry = dataFileFound;
         }
-        else {
+        else { // The file is not in the collection so need to create a new entry
             dataFileEntry = new DataFile({
                 fileName: newFileName
             })
         }
+        // In any case - read the uploaded file into the existing or new entry
         dataFileEntry.fileData = fs.readFileSync(req.file.path);
+        // Save the file in the db
         dataFileEntry.save(function (err) {
-            if (err) {
+            if (err) { // There was an error saving in the db
                 console.log("%s.%s:%s -", __file, __ext, __line, "Error: ", err);
                 return renderError(req, res);
             }
@@ -70,25 +76,27 @@ exports.addFile = function (req, res) {
     });
 };
 
-function timeout(delay) {
-    return new Promise(function (resolve, reject) {
-        setTimeout(resolve, delay);
-    });
-}
-
-exports.getFile = function (fileNames, callback) {
+// Check if a file is available in tmp directory. If not - check if it's available in the db and retrieve it to tmp.
+// If it's neither in tmp or in db, check the same for an alternate file.
+// Also supports checking for only one file - if a second filename isn't provided.
+// The method uses a promise for the result of the check.
+exports.getFile = function (fileNames) {
     return new Promise(
         function (resolve, reject) {
             var fileName = fileNames[0];
-            if (fs.existsSync('/tmp/' + fileName)) { // file already exists
+            if (!fileName) { // safety
+                reject(new Error('Missing filenane'));
+                return;
+            }
+            if (fs.existsSync('/tmp/' + fileName)) { // First file already exists in tmp
                 console.log("%s.%s:%s -", __file, __ext, __line, "File already exists, using it: ", fileName);
                 resolve(fileName);
                 return;
             }
 
-            console.log("%s.%s:%s -", __file, __ext, __line, "File doesn't exist, checking in db: ", fileName);
-            DataFile.findOne({fileName: fileName}, function (err, dataFileFound) {
-                if (err) {
+            console.log("%s.%s:%s -", __file, __ext, __line, "First file doesn't exist, checking in db: ", fileName);
+            DataFile.findOne({fileName: fileName}, function (err, dataFileFound) { // Check the db for first file
+                if (err) { // Error checking in db. This doesn't mean the file isn't there - it means we couldn't check!
                     console.log("%s.%s:%s -", __file, __ext, __line, "Error: ", err);
                     let reason = new Error("Error: ", err);
                     reject(reason);
@@ -101,20 +109,28 @@ exports.getFile = function (fileNames, callback) {
                     resolve(fileName);
                     return;
                 }
-                else {
+                else { // File wasn't found in db either. Check for the second file instead.
                     console.log("%s.%s:%s -", __file, __ext, __line, "File not found in db: ", fileName);
                     let reason = new Error("File not found in db: ", fileName);
 
                     fileName = fileNames[1];
-                    if (fs.existsSync('/tmp/' + fileName)) { // file already exists
+                    if (!fileName) { // Was a second filename provided?
+                        // No second filename provided. Just signal the failure
+                        let reason = new Error("File not found in db: ", fileName);
+                        reject(reason);
+                        return;
+                    }
+
+                    if (fs.existsSync('/tmp/' + fileName)) { // file already exists in tmp
                         console.log("%s.%s:%s -", __file, __ext, __line, "File already exists, using it: ", fileName);
                         resolve(fileName);
                         return;
                     }
 
                     console.log("%s.%s:%s -", __file, __ext, __line, "File doesn't exist, checking in db: ", fileName);
+                    // Check the db for the second file
                     DataFile.findOne({fileName: fileName}, function (err, dataFileFound) {
-                        if (err) {
+                        if (err) { // Error checking in db. This doesn't mean the file isn't there - it means we couldn't check!
                             console.log("%s.%s:%s -", __file, __ext, __line, "Error: ", err);
                             let reason = new Error("Error: ", err);
                             reject(reason);
@@ -127,7 +143,7 @@ exports.getFile = function (fileNames, callback) {
                             resolve(fileName);
                             return;
                         }
-                        else {
+                        else { // File isn't in db either
                             console.log("%s.%s:%s -", __file, __ext, __line, "File not found in db: ", fileName);
                             let reason = new Error("File not found in db: ", fileName);
                             reject(reason);
